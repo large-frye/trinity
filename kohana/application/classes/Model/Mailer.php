955fb5f4610993a2d48a262870889e5c1de8df95
Andrew Frye <a.frye4@gmail.com>
2013-12-11 21:38:02 -0500

    adding mail ability

--- kohana/application/classes/Model/Mailer.php
+++ kohana/application/classes/Model/Mailer.php
@@ -9,25 +9,26 @@ class Model_Mailer extends Model_Base {
 
     	require $_SERVER['DOCUMENT_ROOT'] . '/trinity/PHPMailer/PHPMailerAutoload.php';
 
-    	$this->_host = "smtp.gmail.com";
-    	$this->_port = 587;
+    	$this->_host = 'smtp.gmail.com';
+    	$this->_port = 465;
     	$this->_username = 'a.frye4@gmail.com';
-    	$this->_password = '';
+    	$this->_password = 'mraybcnvbkczlndw';
     }
 
 
 
     public function send_mail($to, $from, $subject, $body, $cc = array(), $bcc = array()) {
         $mail = new PHPMailer();
-        $mail->isSMTP();
-        $mail->SMTPDebug = 0;
+        $mail->IsSMTP();
+        $mail->SMTPDebug = 1;
+        $mail->SMTPAuth = true;
+        $mail->SMTPSecure = 'ssl';
         $mail->Host = $this->_host;
         $mail->Port = $this->_port;
-        $mail->SMTPSecure = 'tls';
-        $mail->SMTPAuth = true;
         $mail->Username = $this->_username;
         $mail->Password = $this->_password;
-        $mail->setFrom('admin@trinity.is', 'Trinity Inspections');
+        $mail->IsHTML = true;
+        $mail->SetFrom('admin@trinity.is', "Trinity Roof Inspections");
         $mail->Subject = $subject;
         $mail->addAddress($to);
         $mail->Body = $body;
