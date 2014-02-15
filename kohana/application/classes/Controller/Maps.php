5d9a2cdf078d315113e335e31c1d86b7259ba660
Darren Holmblad <dholmblad@gmail.com>
2014-02-15 17:51:37 -0500

    add

--- kohana/application/classes/Controller/Maps.php
+++ kohana/application/classes/Controller/Maps.php
@@ -6,29 +6,22 @@ class Controller_Users extends Controller_Account {
 
 	protected $_post = null;
 
-   
-    public function __construct(Kohana_Request $request, Kohana_Response $response) {
-    	parent::__construct($request, $response);
+   public function __construct(Kohana_Request $request, Kohana_Response $response){
+            parent::__construct($request, $response);
+            $this->settings_model=Model::factory('Maps');
 
-    	
-    }
+
+        }
 
     public function before() {
     	parent::before();
-
     	$this->_post = $this->request->post();
     }
+   
 
-
-
-    public function action_index() {
-    	$view = View::factory('maps/index');
-        $this->template->content = $view;
+    public function action_index(){
+       $this->template->content=View::factory('maps/index');
     }
-
-
-
-
 
     public function after() {
     	parent::after();
