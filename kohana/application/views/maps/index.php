bfc36cd6af0c91c59cdd0a911b6a5242ecd246a8
Darren Holmblad <dholmblad@gmail.com>
2014-02-15 18:47:38 -0500

    add

--- kohana/application/views/maps/index.php
+++ kohana/application/views/maps/index.php
@@ -1,1 +1,9 @@
-<div id="map-canvas"></div>
+<div class="section">
+    <div class="box">
+        <div class="title">Maps</div>
+			<div class="content">
+				<div id="map-canvas"></div>
+			</div>
+	    </div>
+	</div>
+</div>
