552bf71d1eae29f72cdd9ea55761d572c6340904
Andrew Frye <a.frye4@gmail.com>
2014-02-17 23:29:46 -0500

    changes to the buttons and events.

--- kohana/application/views/calendar/index.php
+++ kohana/application/views/calendar/index.php
@@ -5,33 +5,26 @@
     <div class="page-header">
         <h1>Trinity Calendar</h1>
     </div>
-    <div class="well">
-        <div class="row-fluid">
-            <div class="span4">
-                <h3>Events</h3>
-                <p>Show the data binding between two differnet calendars using the same event sources.</p>
-                <div class="btn-group calTools">
-                    <button class="btn" ng-click="changeLang()">
-                        {{changeTo}}
-                    </button>
-                    <button class="btn" ng-click="addRemoveEventSource(eventSources,eventSource)">
-                        Toggle Source
-                    </button>
-                    <button type="button" ng-click="addEvent()"><span>Add New Event</span></button>
-                    <button type="submit" name="submit"><span>Save changes</span></button>
+    <section>
+        <div>
+            <h3>Events</h3>
+            <p>Show the data binding between two differnet calendars using the same event sources.</p>
+            <div class="calTools">
+                <button type="button" ng-click="addEvent()"><span>Add New Event</span></button>
+                <button type="submit" name="submit"><span>Save changes</span></button>
+            </div>
+
+                <div class="row">
+                    <ul class="unstyled">
+                        <li ng-repeat="e in events.events">
+                            <div class="left">
+                                <a class="close" ng-click="remove($index)">Delete</a>
+                                 <b> <input type="text" ng-model="e.title"></b> 
+                                 <i>({{e.start | date:"MMM dd"}} - {{e.end | date:"MMM dd"}})</i>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
-
-                        <ul class="unstyled">
-                            <li ng-repeat="e in events.events">
-                                <div class="alert alert-info">
-                                    <a class="close" ng-click="remove($index)">Delete</a>
-                                    <b> <input ng-model="e.title"></b> 
-                                    {{e.start | date:"MMM dd"}} - {{e.end | date:"MMM dd"}}
-                                </div>
-                            </li>
-                        </ul>
-
-                    </div>
 
                     <div onload="renderCalender(myCalendar1);">
                         <div class="alert-success calAlert" ng-show="alertMessage != undefined && alertMessage != ''">
@@ -45,14 +38,14 @@
                         <div class="calendar" ng-model="eventSources" calendar="myCalendar1" config="uiConfig.calendar" ui-calendar="uiConfig.calendar"></div>
                     </div>
                 </div>
-            </div>
             <div id="dialog" title="{{dialog_event.title}}" class="hide">
                 <p>{{dialog_event.title}}</p>
-                <b>Start Time</b><span>{{dialog_event.start}}</span>
-                <b>End Time</b><span>{{dialog_event.end}}</span>
+                <b>Start Time</b><span>{{dialog_event.start | date:'medium'}}</span>
+                <b>End Time</b><span>{{dialog_event.end | date:'medium'}}</span>
                 <b>All Day Event</b><input type="checkbox" value="" />
             </div>
-
+     
         <button type="button" ng-click="saveEvents()"><span>Save Calendar</span></button>
+    </section>
 </div>
 <!-- End Calendar Controller #angular -->
