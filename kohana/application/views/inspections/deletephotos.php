0539decd51f91ba2fcff0e66551fa33668f540b9
Darren Holmblad <dholmblad@gmail.com>
2014-03-03 11:24:17 -0500

    add

---
+++ kohana/application/views/inspections/deletephotos.php
@@ -0,0 +1,40 @@
+<div class="section">
+					<div class="box">
+						<div class="title">
+							Gallery
+							<span class="hide"></span>
+						</div>
+						<div class="content nopadding">
+							<div class="gallery">
+							<div class="photoCatContainer">
+<?php
+
+				echo '<table>';
+						$count = count($photos);
+
+							$rwCnt=0;
+                                for ($i = 0; $i < $count; $i++) {
+                                	if($rwCnt==0){
+                                		echo "<tr>";
+                                	}
+
+                                	if($rwCnt<6){
+                                			echo "<td><div class=photoThumView'><img class='photoImgView' src=\"" . str_replace('..', '', $photos[$i]->fileLocation) . "\" style='width: 80px; height: 80px;' /></p></div></td>";
+                                	$rwCnt++;
+                                	}else {
+                                		echo "<td><div  class=photoThumView'><img class='photoImgView' src='".str_replace('..', '', $photos[$i]->fileLocation)."' style='width: 80px; height: 80px;' /></p></div></td></tr>";
+                                	$rwCnt=0;
+                                	}
+                                	
+                                }
+
+                 echo '</table>';
+?>
+</div>
+
+
+								
+							</div>
+						</div>
+					</div>
+				</div>
