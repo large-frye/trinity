2b8da7c4a3844d09356321ac509b129561f44607
Darren Holmblad <dholmblad@gmail.com>
2014-03-03 14:11:20 -0500

    add

--- kohana/application/classes/Model/Inspections.php
+++ kohana/application/classes/Model/Inspections.php
@@ -928,10 +928,8 @@ class Model_Inspections extends Model_Base {
             $tmpAry = explode(',', $post[$i]);
             $pLoc = $tmpAry[1];
             $id = $tmpAry[0];
-           // echo getcwd();
-           // die();
            echo $id;
-           // DB::delete('inspection_photos')->where('id', '=', ':id')->parameters(array(':id' => $id))->execute($this->db);
+           DB::delete('inspection_photos')->where('id', '=', ':id')->parameters(array(':id' => $id))->execute($this->db);
            try {
             unlink('..'.$pLoc);
             }  catch (Exception $e) {
