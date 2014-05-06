6ffe90931465bb0f570d6b73ade51d8695a0db61
Darren Holmblad <dholmblad@gmail.com>
2014-05-05 20:12:16 -0400

    add

--- kohana/application/classes/Model/Inspections.php
+++ kohana/application/classes/Model/Inspections.php
@@ -948,6 +948,8 @@ class Model_Inspections extends Model_Base {
         }
     }
         public function delete_all_photos($id){
+            echo $id;
+            die();
             $allPhotos = DB::query(Database::SELECT, 'SELECT p.*, c.name from inspection_photos p 
                 LEFT JOIN categories c
                 ON p.category_id = c.id 
@@ -959,9 +961,9 @@ class Model_Inspections extends Model_Base {
         $arrySize = count($allPhotos);
         for ($i = 0; $i < $arrySize; $i++) {
            
-             DB::delete('inspection_photos')->where('id', '=', ':id')->parameters(array(':id' => $allPhotos[$i]->id))->execute($this->db);
+          //   DB::delete('inspection_photos')->where('id', '=', ':id')->parameters(array(':id' => $allPhotos[$i]->id))->execute($this->db);
            try {
-            unlink('..'.$allPhotos[$i]->fileLocation);
+          //  unlink('..'.$allPhotos[$i]->fileLocation);
             }  catch (Exception $e) {
                // echo 'Caught exception: ',  $e->getMessage(), "\n";
             }
