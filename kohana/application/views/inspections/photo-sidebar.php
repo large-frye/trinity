7217f4cb282922cd0c609df823fb2c5c1940f706
Andrew Frye <a.frye4@gmail.com>
2014-05-27 10:51:28 -0400

    testing wind/hail exp damages error on 122

--- kohana/application/views/inspections/photo-sidebar.php
+++ kohana/application/views/inspections/photo-sidebar.php
@@ -22,6 +22,29 @@
                       <li class="<?php echo Request::current()->action() == 'view' ? 'current' : null; ?>">
                             <a href="/inspections/deletephotos/<?php echo Request::current()->param('id'); ?>">
                                 Delete Photos</a></li>
+
+                                <li class="current">
+                                                <a href="/workorders/view/<?php echo Request::current()->param('id'); ?>">View Work Order</a>
+                                        </li>
+                                        <li class="current">
+                                                <a href="/workorders/edit/<?php echo Request::current()->param('id'); ?>">Edit Work Order</a>
+                                        </li>
+                                        <li class="current">
+                                                <a href="/inspections/form/<?php echo Request::current()->param('id'); ?>">Inspection Form</a>
+                                        </li>
+                                        <li class="current">
+                                                <a href="/inspections/viewphotos/<?php echo Request::current()->param('id'); ?>">Add/View Photos</a>
+                                        </li>
+                                        <li class="current">
+                                                <a href="/invoice/index/<?php echo Request::current()->param('id'); ?>">View Invoice</a>
+                                        </li>
+                                        <li class="current">
+                                                <a href="/workorders/report/<?php echo Request::current()->param('id'); ?>">View/Generate Report</a>
+                                        </li>
+
+                                        <li class="current">
+                                            <a href="/workorders/submit">Submit New Inspection</a>
+                                        </li>
                     </ul>
             </li>
         </ul>
