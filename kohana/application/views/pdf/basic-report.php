0e959d3743881e1ecbd25580198ac86dfc562540
Andrew Frye <a.frye4@gmail.com>
2015-04-13 20:27:09 -0400

    update git ignore and change verbiage for tony

--- kohana/application/views/pdf/basic-report.php
+++ kohana/application/views/pdf/basic-report.php
@@ -68,7 +68,7 @@ ul
     font:14px;
 }
 .damageLi{
-  
+
   font:15px;
 }
 .damageUL{
@@ -90,7 +90,7 @@ ul
 .imgDiv{
   display: block;
   padding: 10px;
-  margin-top: 50px; 
+  margin-top: 50px;
 
 }
 .parentCatHead{
@@ -124,7 +124,7 @@ ul
 
 <div id="page-wrap">
 <div class="top-bar">
-    <img src="<?php echo $_SERVER['DOCUMENT_ROOT'] . '/assets/gfx/logo-icon.png'; ?>" width="100" height="100" alt="test" style="text-align:center;">
+    <img src="<?php echo $_SERVER['DOCUMENT_ROOT'] . '/assets/gfx/logo-icon.png';?>" width="100" height="100" alt="test" style="text-align:center;">
     <table style="margin-top: -75px">
         <tr><th class="header">Trinity Inspections, LLC</th></tr>
         <tr><td class="center">P.O. Box 938</td></tr>
@@ -143,7 +143,7 @@ ul
     <tr><th>Insurance Company:</th><td class="border"><?php echo $inspection_data['company_name'];?></td></tr>
     <tr><th>Date:</th><td class="border">&nbsp;<?php echo $inspection_data['date_of_inspection'];?></td></tr>
 </table>
-<?php 
+<?php
 
 $collateral_damage_header = isset($report_data['damages']['collateral_damage_header']) ? $report_data['damages']['collateral_damage_header'] : null;
 $collateral_damages = isset($report_data['damages']['collateral_damage_header']['collateral_damages']) ? $report_data['damages']['collateral_damage_header']['collateral_damages'] : null;
@@ -153,94 +153,95 @@ $collateral_damages = isset($report_data['damages']['collateral_damage_header'][
 <h4 class="row-header">GROUND INSPECTION</h4>
 <!-- <p>During our ground level walk around inspection of loss to the following building materials that may be more susceptible to wind or hail:</p>-->
 <p>During our ground level walk around inspection of the loss <span class="blue">
-  <?php echo isset($collateral_damage_header) && !empty($collateral_damage_header) ? "we did find collateral damage" : "we did not find collateral damage"; ?></span> 
+<?php echo isset($collateral_damage_header) && !empty($collateral_damage_header) ? "we did find collateral damage" : "we did not find collateral damage";?></span>
    to the following building materials that may be more susceptible to wind or hail.</p>
-<p class="blue"><?php echo isset($collateral_damages) && !empty($collateral_damages) ? implode(', ', $collateral_damages) : null; ?></p>
-<?php if(isset($report_data['damages']['collateral_damamges_comments']) && trim($report_data['damages']['collateral_damamges_comments']) != "") {
-    echo "<h4>Collateral Damages Comments: </h4><p>" . $report_data['damages']['collateral_damamges_comments'] . "</p>";
+<p class="blue"><?php echo isset($collateral_damages) && !empty($collateral_damages) ? implode(', ', $collateral_damages) : null;?></p>
+<?php if (isset($report_data['damages']['collateral_damamges_comments']) && trim($report_data['damages']['collateral_damamges_comments']) != "") {
+	echo "<h4>Collateral Damages Comments: </h4><p>" . $report_data['damages']['collateral_damamges_comments'] . "</p>";
 }
 ?>
 <br>
 <h4 class="row-header">ROOF INSPECTION</h4>
 <?php
-$windTotal=0; 
-$hailTotal=0;
- foreach ($report_data['damages'] as $damage => $damages) { 
-          if (preg_match('/header/', $damage) && !preg_match('/collateral/', $damage)) { ?>
-              <h5 class="red"><?php echo strtoupper(str_replace('_header', '', $damage.' damage:')); 
-               if(preg_match('/wind/', $damage)){
-                echo '<p class="sectionDescrip">Our wind damage inspection consists of inspecting every roof slope to verify any and all wind damaged components to all types of
+$windTotal = 0;
+
+$hailTotal = 0;
+foreach ($report_data['damages'] as $damage => $damages) {
+
+	if (preg_match('/header/', $damage) && !preg_match('/collateral/', $damage)) {
+		?>
+              <h5 class="red"><?php echo strtoupper(str_replace('_header', '', $damage . ' damage:'));
+
+		if (preg_match('/wind/', $damage)) {
+			echo '<p class="sectionDescrip">Our wind damage inspection consists of inspecting every roof slope to verify any and all wind damaged components to all types of
 roofing systems.</p>';
-              } elseif(preg_match('/hail/', $damage)){
-              echo '<p class="sectionDescrip">Our hail damage inspection consists of looking on all directional slopes for granular displacement on the shingles that are about the
-                  size in diameter of a dime supported by mat fracture. These areas of granular displacement must be across the entire directional
+		} elseif (preg_match('/hail/', $damage)) {
+			echo '<p class="sectionDescrip">Our hail damage inspection consists of looking on all directional slopes for granular displacement on the shingles that are about the
+                  size in diameter of a dime, which may or may not be supported by mat fracture. These areas of granular displacement must be across the entire directional
                   slope that we are assessing (which is a characteristic of hail damage). We use a 10’ X 10’ test square on all 4 directional slopes to test
                   the statistical average of hail.</p>';
-                     }
+		}
 
-              ?></h5>
+		?></h5>
               <div class="damageUL">
               <ul>
-              <?php 
-              
-             
-                $directions =array("North", "South", "East", "West", "(Front)", "(Rear)", "(Left)", "(Right)", "NorthEast", "SouthEast", "SouthWest", "NorthWest");
-                foreach ($damages as $tmp) {
-                    $finalStr='';
-                    if (is_array($tmp)) {
-                        foreach ($tmp as $key => $value) {
-                            foreach (explode(' ', $value) as $opt) {
-                                if (preg_match('/[0-9]+/', $opt)) {
-                                    if (preg_match('/wind/', $damage)) {
-                                        $windTotal = $windTotal + $opt;
-                                    } else if (preg_match('/hail/', $damage)) {
-                                        $hailTotal = $hailTotal + $opt;
-                                    }
+<?php
 
-                                    $finalStr .= " <span style=\"color:red\">" . $opt . "</span>";
-                                } else {
-                                    $finalStr .= " " . $opt;
-                                }
-                            }
+		$directions = array("North", "South", "East", "West", "(Front)", "(Rear)", "(Left)", "(Right)", "NorthEast", "SouthEast", "SouthWest", "NorthWest");
+		foreach ($damages as $tmp) {
+			$finalStr = '';
+			if (is_array($tmp)) {
+				foreach ($tmp as $key => $value) {
+					foreach (explode(' ', $value) as $opt) {
+						if (preg_match('/[0-9]+/', $opt)) {
+							if (preg_match('/wind/', $damage)) {
+								$windTotal = $windTotal + $opt;
+							} else if (preg_match('/hail/', $damage)) {
+								$hailTotal = $hailTotal + $opt;
+							}
 
-                            $finalStr .= "<br />";
-                        }
-                    } else {
-                        foreach(explode(' ',$tmp) as $st) {
-                            if(is_numeric($st)){
-                                if (preg_match('/wind/', $damage)) {
-                                    $windTotal = $windTotal + $st;
-                                }
-                                else if (preg_match('/hail/', $damage)) {
-                                    $hailTotal = $hailTotal + $st;
-                                }
+							$finalStr .= " <span style=\"color:red\">" . $opt . "</span>";
+						} else {
+							$finalStr .= " " . $opt;
+						}
+					}
 
-                                $finalStr = $finalStr . ' <span class="redTxt">' . $st . '</span>';
-                            } else {
-                                $finalStr=$finalStr.' '.$st;
-                            }
-                        }
-                    }
+					$finalStr .= "<br />";
+				}
+			} else {
+				foreach (explode(' ', $tmp) as $st) {
+					if (is_numeric($st)) {
+						if (preg_match('/wind/', $damage)) {
+							$windTotal = $windTotal + $st;
+						} else if (preg_match('/hail/', $damage)) {
+							$hailTotal = $hailTotal + $st;
+						}
 
-                    $tmp=$finalStr;
-                    foreach ($directions as $value) {
-                        $tmp = str_replace($value, '<span class="redTxt">'.$value.'</span>', $tmp);
-                    }
+						$finalStr = $finalStr . ' <span class="redTxt">' . $st . '</span>';
+					} else {
+						$finalStr = $finalStr . ' ' . $st;
+					}
+				}
+			}
 
-                    if (preg_match('/hail-damaged shingles/', $tmp)) {
-                        $tmp = str_replace('hail-damaged shingles', 'hail-damaged shingles per test square', $tmp);
-                    }
-                 ?>
-                  <li class="damageLi"><?php echo $tmp; ?></li>
-              <?php 
-            } ?>
-            </ul>
+			$tmp = $finalStr;
+			foreach ($directions as $value) {
+				$tmp = str_replace($value, '<span class="redTxt">' . $value . '</span>', $tmp);
+			}
+
+			if (preg_match('/hail-damaged shingles/', $tmp)) {
+				$tmp = str_replace('hail-damaged shingles', 'hail-damaged shingles per test square', $tmp);
+			}
+			?>
+                  <li class="damageLi"><?php echo $tmp;?></li>
+<?php
+}?>
+</ul>
           </div>
 
-    <?php }
+<?php }
 
-  } ?>
-
+}?>
 </div>
 <!-- <div class="page-break"></div> -->
 <div style="ground-inspection">
@@ -249,13 +250,13 @@ roofing systems.</p>';
 <?php
 
 if (isset($report_data['damages']['general_comments'])) {
-    if (trim($report_data['damages']['general_comments']) == null || trim($report_data['damages']['general_comments']) == "") {
-        echo "There were no comments left about this report.";
-    } else {
-        echo "&nbsp;" . $report_data['damages']['general_comments'];
-    }
+	if (trim($report_data['damages']['general_comments']) == null || trim($report_data['damages']['general_comments']) == "") {
+		echo "There were no comments left about this report.";
+	} else {
+		echo "&nbsp;" . $report_data['damages']['general_comments'];
+	}
 } else {
-    echo "No comments.";
+	echo "No comments.";
 }
 
 ?>
@@ -269,4 +270,6 @@ if (isset($report_data['damages']['general_comments'])) {
 
 <?php // echo isset($sketch) ? "<img src=\"" . $sketch->fileLocation . "\" alt=\"sketch\" width=\"850\" class=\"sketch-helper\" style=\"margin-right: auto; margin-left: auto;\" />" : null;  ?>
 </body>
-</html>  
+</html>
+
+
